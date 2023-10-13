39b8f8357121c7530d6b2b85ccbe29639952fd31
SriGanesh01 <psriganesh2005@gmail.com>
2023-10-13 13:26:40 +0530

    imported datas

--- DataScienceAndMachineLearning/Data.py
+++ DataScienceAndMachineLearning/Data.py
@@ -1,1 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sklearn
 print("Hello World")
+
+#importing the dataset
+
+df = pd.read_csv('DataScienceAndMachineLearning\Data_Crypto_1\consolidated_coin_data.csv')
+print(df.head(10))
